5c004e56799e6dc06f950ee73d746d5f3b7e4737
CJavat <cdpm98@hotmail.com>
2024-07-05 06:23:24 -0600

    SECCIÓN 3 - TERMINADA

--- nest-gpt/src/gpt/use-cases/orthography.use-case.ts
+++ nest-gpt/src/gpt/use-cases/orthography.use-case.ts
@@ -12,15 +12,35 @@ export const orthographyCheckUseCase = async (
 
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: 'Eres un asistente muy util.' },
+      {
+        role: 'system',
+        content: `
+        Te serán proveídos textos con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        Debes de responder en formato JSON,
+        Tu tarea es corregirlos y retornar información soluciones,
+        también debes de dar un porcentaje de acierto por el usuario,
+
+
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+        
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], // ['error -> solución']
+          message: string, // Usa emojis y texto para felicitar al usuario
+        }
+        `,
+      },
       { role: 'user', content: prompt },
     ],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o',
     temperature: 0.3,
     max_tokens: 150,
   });
 
-  console.log(completion);
+  // console.log(completion);
+  const jsonResp = JSON.parse(completion.choices[0].message.content);
 
-  return completion.choices[0];
+  return jsonResp;
 };
